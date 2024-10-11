61ab27b3a4fbe47813ab44e7bfa67da8aa29a4cb
AbdulrahmanAbozaid <diver72003@gmail.com>
2024-10-11 22:18:45 +0300

    Done Problem

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Lockboxes
+"""
+from collections import deque
+
+
+def canUnlockAll(boxes):
+    """A method that determines if all the boxes can be opened."""
+    if not len(boxes):
+        return True
+
+    queue = deque()
+    visited = [False] * len(boxes)
+    queue.append(boxes[0])
+    visited[0] = True
+
+    while len(queue):
+        curr = queue.popleft()
+
+        for v in curr:
+            if not visited[v]:
+                queue.append(boxes[v])
+                visited[v] = True
+
+    return all(visited)
